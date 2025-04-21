5c911e7000b13a372415bad416aec48a0bcdd82b
parveen-gitcode <dudekulaparveenatp@gmail.com>
2025-04-21 18:11:30 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import './Cart.css'
 const Cart = () => {
+  const Cart = () => {
+  const {
+    url,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    food_list,
+    getTotalCartAmount,
+  } = useContext(StoreContext);
+    
   return (
     <div>
       
