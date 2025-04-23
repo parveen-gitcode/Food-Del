d407e2d97e0e6e7f022b88bac389e8fdc79b68e3
parveen-gitcode <dudekulaparveenatp@gmail.com>
2025-04-23 21:45:57 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,192 @@
-import React from 'react'
-import './PlaceOrder.css'
-const PlaceOrder = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useContext, useEffect, useState } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default PlaceOrder
+const PlaceOrder = () => {
+  const navigate = useNavigate();
+  const {
+    url,
+    token,
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+  } = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
+      // Redirect to cart page if cart is empty
+      navigate("/cart");
+    }
+  }, [token]);
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+
+    //serup cart items
+    const orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let intemInfo = item;
+        intemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(intemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+
+    //place order API
+    const response = await axios.post(`${url}api/order/place`, orderData, {
+      headers: { token },
+    });
+
+    //Display the response
+    if (response.data.success) {
+      //Get session url
+      const { session_url } = response.data;
+      console.log(session_url);
+
+      window.location.replace(session_url);
+    } else {
+      alert("Error");
+      console.error(response.data.message);
+    }
+  };
+
+  return (
+    <form onSubmit={placeOrder} className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input
+            required
+            name="firstName"
+            value={data.firstName}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            required
+            name="lastName"
+            value={data.lastName}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="Last name"
+          />
+        </div>
+        <input
+          required
+          name="email"
+          value={data.email}
+          onChange={onChangeHandler}
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          required
+          name="street"
+          value={data.street}
+          onChange={onChangeHandler}
+          type="text"
+          placeholder="Street"
+        />
+        <div className="multi-fields">
+          <input
+            required
+            name="city"
+            value={data.city}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="City"
+          />
+          <input
+            required
+            name="state"
+            value={data.state}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="multi-fields">
+          <input
+            required
+            name="zipcode"
+            value={data.zipcode}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="Zip"
+          />
+          <input
+            required
+            name="country"
+            value={data.country}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          required
+          name="phone"
+          value={data.phone}
+          onChange={onChangeHandler}
+          type="text"
+          placeholder="Phone"
+        />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() > 0 ? 2 : 0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() > 0 ? getTotalCartAmount() + 2 : 0}</b>
+            </div>
+          </div>
+          <button type="submit">PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default PlaceOrder;
